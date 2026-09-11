425be91b9e7e65803df62d88d444a9287051e683
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2026-09-11 13:26:09 +1000

    Added negative testing but bug testing

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test('User can login', async({page})=>{
-
     await page.goto('https://www.saucedemo.com/');
-
     await page.getByTestId('username').fill('standard_user');
-
     await page.getByTestId('password').fill('secret_sauce');
-
     await page.getByRole('button', {name: 'Login'}).click();
 
     await expect(page).toHaveURL(/inventory/);
 });
+
+
+// test('User cannot login', async({page})=>{
+//     await page.goto('https://www.saucedemo.com/');
+//     await page.getByTestId('username').fill('tester');
+//     await page.getByTestId('password').fill('pass');
+
+//     await expect(page.getByTestId('error')).toBeVisible();
+
+// })
